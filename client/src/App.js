c0db75ce3b29520dfcc968bbdcca626c78ff4914
Turan Ketene <turank97@gmail.com>
2019-12-20 20:34:33 -0800

    started the react portion by completing the routes in the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavTabs/>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </Router>
+      )
+  }
 }
 
 export default App;
